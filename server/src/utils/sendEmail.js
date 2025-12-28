caa6a46a04a081dca7f2de4e1db36187d5f666f9
Pablo de la Garza <pdv_88@hotmail.com>
2025-12-28 17:27:51 -0600

    feat: add email sending utility using Nodemailer and Handlebars templates.

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -11,6 +11,9 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
+        tls: {
+            rejectUnauthorized: false
+        }
     },
 });
 
